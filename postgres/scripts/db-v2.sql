73f33b0bd1e7452d7166b655c6841d720cdaaf44
spirit174 <zemtsov04@yandex.ru>
2025-10-11 18:07:19 +0300

    try

--- postgres/scripts/db-v2.sql
+++ postgres/scripts/db-v2.sql
@@ -8,6 +8,19 @@ CREATE DATABASE loyalties;
 GRANT ALL PRIVILEGES ON DATABASE loyalties TO program;
 
 \c reservations;
+CREATE TABLE hotels
+(
+    id        SERIAL PRIMARY KEY,
+    hotel_uid uuid         NOT NULL UNIQUE,
+    name      VARCHAR(255) NOT NULL,
+    country   VARCHAR(80)  NOT NULL,
+    city      VARCHAR(80)  NOT NULL,
+    address   VARCHAR(255) NOT NULL,
+    stars     INT,
+    price     INT          NOT NULL
+);
+
+
 CREATE TABLE reservation
 (
     id              SERIAL PRIMARY KEY,
@@ -19,18 +32,6 @@ CREATE TABLE reservation
         CHECK (status IN ('PAID', 'CANCELED')),
     start_date      TIMESTAMP WITH TIME ZONE,
     end_data        TIMESTAMP WITH TIME ZONE
-);
-
-CREATE TABLE hotels
-(
-    id        SERIAL PRIMARY KEY,
-    hotel_uid uuid         NOT NULL UNIQUE,
-    name      VARCHAR(255) NOT NULL,
-    country   VARCHAR(80)  NOT NULL,
-    city      VARCHAR(80)  NOT NULL,
-    address   VARCHAR(255) NOT NULL,
-    stars     INT,
-    price     INT          NOT NULL
 );
 
 INSERT INTO public.hotels(id, hotel_uid, name, country, city, address, stars, price) VALUES (
